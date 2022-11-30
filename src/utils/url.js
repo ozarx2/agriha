88352f2570602cf51b09f8a7808cee59b17d0e97
YazeenShiras <yaseenkmr786@gmail.com>
2022-11-30 16:15:33 +0530

    integration working progressed

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,4 +1,4 @@
 // var api_url = "https://arclif-agriha.herokuapp.com";
-var api_url = "https://agriha-server-dot-agriha-services.uc.r.appspot.com/";
+var api_url = "https://agriha-server-dot-agriha-services.uc.r.appspot.com";
 
 export default api_url;

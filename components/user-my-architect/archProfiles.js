95a665ad98809cb17db8ab4ae500eaf5419c50ce
YazeenShiras <yaseenkmr786@gmail.com>
2022-12-06 15:02:19 +0530

    auth checking

--- components/user-my-architect/archProfiles.js
+++ components/user-my-architect/archProfiles.js
@@ -108,30 +108,38 @@ const FnArchProfiles = () => {
                     </div>
                   </div>
                   <div>
-                    <div className={styles.archLocationMain}>
-                      <img
-                        src="/img/architect/locationblue.svg"
-                        alt="locationblue.svg"
-                        className={styles.locationIconGreen}
-                      />
-                      <div className={styles.archlocation}>{items?.location}</div>
-                    </div>
+                    {items?.location ? (
+                      <div className={styles.archLocationMain}>
+                        <img
+                          src="/img/architect/locationblue.svg"
+                          alt="locationblue.svg"
+                          className={styles.locationIconGreen}
+                        />
+                        <div className={styles.archlocation}>{items?.location}</div>
+                      </div>
+                    ) : (
+                      ""
+                    )}
                     <div className={styles.category}>
                       <div>Architect</div>
                     </div>
                     <div className={styles.categorySubhead}>
                       <div>{items?.bio}</div>
                     </div>
-                    <Link href={`/user-architect-about/${items._id}`} passHref>
-                      <div className={styles.readMore}>
-                        <div>Read more</div>
-                        <img
-                          src="/img/architect/downarrow.svg"
-                          alt="downarrow.svg"
-                          className={styles.categoryDownIcon}
-                        />
-                      </div>
-                    </Link>
+                    {items?.bio ? (
+                      <Link href={`/user-architect-about/${items._id}`} passHref>
+                        <div className={styles.readMore}>
+                          <div>Read more</div>
+                          <img
+                            src="/img/architect/downarrow.svg"
+                            alt="downarrow.svg"
+                            className={styles.categoryDownIcon}
+                          />
+                        </div>
+                      </Link>
+                    ) : (
+                      ""
+                    )}
                   </div>
                 </div>
               </div>

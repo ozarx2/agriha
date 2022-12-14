f7a77fc62870237a43ec599c602c9e876703e981
aZ <az@localhost.localdomain>
2022-12-14 12:49:13 +0530

    hidden api 2

---
+++ components/global/fetchWithCache.js
@@ -0,0 +1,14 @@
+import cacheData from "memory-cache";
+
+async function fetchWithCache(url, options) {
+  const value = cacheData.get(url);
+  if (value) {
+    return value;
+  } else {
+    const hours = 24;
+    const res = await fetch(url, options);
+    const data = await res.json();
+    cacheData.put(url, data, hours * 1000 * 60 * 60);
+    return data;
+  }
+}
--- pages/api/home.js
+++ pages/api/home.js
@@ -1,19 +1,57 @@
-import React, { useRef, useState, useEffect, useContext } from "react";
 import api_url from "../../src/utils/url";
 
-export default function test(req, res) {
-  async function getAllProjects() {
-    const response = await fetch(`${api_url}/projects/getallprojects`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await response.json();
-    if (data) {
-      const withArchitect = data.data.filter((res) => res?.architect_id);
-      res.status(200).json(withArchitect);
-    }
+var cache = {};
+function getPageFromCache(url, callback) {
+  if (cache[url]) {
+    callback(undefined, cache[url]);
+  } else {
+    callback();
   }
-  getAllProjects();
+}
+function setPageToCache(url, content) {
+  cache[url] = content;
+}
+
+export default function test(req, res) {
+  getPageFromCache(req.url, async function (err, content) {
+    if (err) return req.next(err);
+    if (content) {
+      console.log("working");
+      res.send(content);
+    } else {
+      const response = await fetch(`${api_url}/projects/getallprojects`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      if (data) {
+        const withArchitect = data.data.filter((res) => res?.architect_id);
+        setPageToCache(req.url, withArchitect);
+        res.status(200).json(withArchitect);
+      }
+      // res.render("index.jade", { title: "My Site" }, function (err, content) {
+      //   // Render handler
+      //   if (err) return req.next(err);
+      //   setPageToCache(req.url, page);
+      //   res.send(content);
+      // });
+    }
+  });
+
+  // async function getAllProjects() {
+  //   const response = await fetch(`${api_url}/projects/getallprojects`, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
+  //   const data = await response.json();
+  //   if (data) {
+  //     const withArchitect = data.data.filter((res) => res?.architect_id);
+  //     res.status(200).json(withArchitect);
+  //   }
+  // }
+  // getAllProjects();
 }

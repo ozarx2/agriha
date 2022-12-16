b429221ce82ee2a752b66a3ba3b82cdcb11c3c9d
YazeenShiras <yaseenkmr786@gmail.com>
2022-12-16 19:06:59 +0530

    new

--- components/user-my-project/foldercard.js
+++ components/user-my-project/foldercard.js
@@ -5,17 +5,15 @@ import styles from "./folder.module.css";
 
 const FnFolderCard = ({ title, date, fileLists }) => {
   return (
-    <div className={styles.fileUploadsSection}>
-      <div className={styles.fileUploadDiv}>
-        <div className={styles.fileUpload}>
-          <img src="/img/my-project-user/Folder.svg" alt="folder.svg" />
-          <div className={styles.fileFolderTitle}>{title}</div>
-          <div className={styles.fileFolderdate}>{moment(date).format("l")}</div>
-        </div>
-        {/* <div>
+    <div className={styles.fileUploadDiv}>
+      <div className={styles.fileUpload}>
+        <img src="/img/my-project-user/Folder.svg" alt="folder.svg" />
+        <div className={styles.fileFolderTitle}>{title}</div>
+        <div className={styles.fileFolderdate}>{moment(date).format("l")}</div>
+      </div>
+      {/* <div>
             <img src="/img/my-project-user/more.svg" alt="more.svg" className={styles.more} />
         </div> */}
-      </div>
     </div>
   );
 };

c2644d0b3137aded2619a28afe7e542bcc40942b
aZ <az@localhost.localdomain>
2022-12-19 18:29:05 +0530

    share architect profile

--- components/user-common/share-popup.js
+++ components/user-common/share-popup.js
@@ -3,6 +3,7 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useRouter } from "next/router";
 import React, { useState, useEffect, useContext } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import { StoreContext } from "../../components/StoreContext";
 import windowSize from "../windowRes";
 
@@ -14,15 +15,22 @@ export default function SharePopup() {
   const [Store] = useContext(StoreContext);
   const setSharePopup = Store.setSharePopup;
 
+  const [url, setUrl] = useState("");
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setUrl(window.location.href);
+    }
+  }, []);
+
   return (
     <>
       <div id="ArchitectSelectPopupOuter" className={styles.ArchitectSelectPopupOuter}>
         <div onClick={() => setSharePopup(false)} className={styles.ArchitectSelectPopupClose}></div>
         <div className={styles.ArchitectSelectPopupInner}>
           {windowRes.innerWidth >= 767 ? (
-            <div className={styles.desktop}>{ArchitectSelectPopupContent()}</div>
+            <div className={styles.desktop}>{ArchitectSelectPopupContent({ url })}</div>
           ) : (
-            <div className={styles.mobile}>{ArchitectSelectPopupContent()}</div>
+            <div className={styles.mobile}>{ArchitectSelectPopupContent({ url })}</div>
           )}
         </div>
       </div>
@@ -30,21 +38,48 @@ export default function SharePopup() {
   );
 }
 
-const ArchitectSelectPopupContent = () => {
+const ArchitectSelectPopupContent = ({ url }) => {
   const [Store] = useContext(StoreContext);
   const setSharePopup = Store.setSharePopup;
 
-  const router = useRouter();
+  const [copy, setCopy] = useState(false);
+
+  const copiedlink = () => {
+    setCopy(true);
+    setInterval(function () {
+      setSharePopup(false);
+    }, 1000);
+  };
 
   return (
     <>
       <div className={styles.popupouter}>
-        <div className={styles.profiles}></div>
-        <div className={styles.line}></div>
-        <div className={styles.buttons}>
-          <div className={styles.continue} onClick={() => setSharePopup(false)}>
-            close
+        <div className={styles.popupinner}>
+          <h4>Share to social media platform</h4>
+          <div className={styles.social}>
+            <a target="_blank" href={`https://web.whatsapp.com/send?text=${url}`}>
+              <img src="/img/icons/w.svg" alt="WhatsApp" />
+              <span>WhatsApp</span>
+            </a>
+            <a target="_blank" href={`https://twitter.com/share?text=&url=${url}`}>
+              <img src="/img/icons/t.svg" alt="WhatsApp" />
+              <span>Twitter</span>
+            </a>
+            <a target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${url}`}>
+              <img src="/img/icons/f.svg" alt="WhatsApp" />
+              <span>Facebook</span>
+            </a>
           </div>
+          <div className={styles.url}>
+            <input id="urlInput" type="text" value={url} readonly />
+            <CopyToClipboard text={url} onCopy={() => copiedlink()}>
+              <a>Copy link</a>
+            </CopyToClipboard>
+          </div>
+          {copy ? <div className={styles.copy}>Link copied to clipboard</div> : ""}
+        </div>
+        <div className={styles.close} onClick={() => setSharePopup(false)}>
+          <img src="/img/architect-dashboard/modal/close.svg" alt="Close" />
         </div>
       </div>
     </>
---
+++ pages/view-requirement/choose-plan/index.js
@@ -0,0 +1,49 @@
+import { useRouter } from "next/router";
+import React, { useEffect, useContext } from "react";
+import { StoreContext } from "../../../components/StoreContext";
+import Head from "next/head";
+import ChoosePlanMain from "../../../components/requirements/ChoosePlanMain";
+import AgrihaLandingHeaderNoSearch from "../../../components/user-common/header-ns";
+import ProfilePopup from "../../../components/user-common/profile-popup";
+
+import styles from "./index.module.css";
+
+const ChoosePlan = () => {
+  const [Store] = useContext(StoreContext);
+
+  const profilePopup = Store.profilePopup;
+
+  const router = useRouter();
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    if (userId) {
+    } else {
+      router.push("/");
+    }
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Choose-Plan - Agriha</title>
+        <meta name="description" content="Agriha Landing page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>
+        <div className={styles.container_outer}>
+          <div className={styles.container_inner}>
+            <div className={styles.header}>
+              <AgrihaLandingHeaderNoSearch />
+            </div>
+            <div className={styles.main}>
+              <ChoosePlanMain />
+            </div>
+          </div>
+        </div>
+      </div>
+      {profilePopup ? <ProfilePopup /> : ""}
+    </>
+  );
+};
+
+export default ChoosePlan;

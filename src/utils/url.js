c7c33c98c1dbd3833679253889df93fca714d480
aZ <az@localhost.localdomain>
2022-12-21 18:17:50 +0530

    error fixing

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,4 +1,4 @@
-var api_url = "https://agrihav2-test.onrender.com";
-// var api_url = "https://agriha-staging.onrender.com";
+// var api_url = "https://agrihav2-test.onrender.com";
+var api_url = "https://agriha-staging.onrender.com";
 
 export default api_url;

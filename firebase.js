8f302e547b235d1acd1cdcbef3cbfad700d536fb
YazeenShiras <yaseenkmr123@gmail.com>
2023-01-08 16:45:51 +0530

    firebase config changed

--- firebase.js
+++ firebase.js
@@ -5,14 +5,26 @@ import { getStorage } from "firebase/storage";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB0csftk73F_f0QG8586CwMBXTAkoW1Jpc",
+//   authDomain: "arclif-socialmedia-backend.firebaseapp.com",
+//   projectId: "arclif-socialmedia-backend",
+//   storageBucket: "arclif-socialmedia-backend.appspot.com",
+//   messagingSenderId: "903847952391",
+//   appId: "1:903847952391:web:522cf8212180cc17e5aaa2",
+//   measurementId: "G-W5R6X58EBL",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyB0csftk73F_f0QG8586CwMBXTAkoW1Jpc",
-  authDomain: "arclif-socialmedia-backend.firebaseapp.com",
-  projectId: "arclif-socialmedia-backend",
-  storageBucket: "arclif-socialmedia-backend.appspot.com",
-  messagingSenderId: "903847952391",
-  appId: "1:903847952391:web:522cf8212180cc17e5aaa2",
-  measurementId: "G-W5R6X58EBL",
+  apiKey: "AIzaSyCizhofwSv7K4-f2HhdGlqQCrFKbAkSCko",
+  authDomain: "arclifimg.firebaseapp.com",
+  projectId: "arclifimg",
+  storageBucket: "arclifimg.appspot.com",
+  messagingSenderId: "655745297713",
+  appId: "1:655745297713:web:7f64c8eed80baf844c1410",
+  measurementId: "G-0FP28ESW7Q"
 };
 
 // Initialize Firebase

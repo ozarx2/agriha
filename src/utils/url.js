cf1905348276807ce4ce0d626e3bc3785a39aa3c
YazeenShiras <yaseenkmr786@gmail.com>
2023-03-01 16:25:50 +0530

    url change

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,6 +1,6 @@
 // var api_url = "https://agrihav2-test.onrender.com";
 // var api_url = "https://agriha-staging.onrender.com";
 // var api_url = "https://agriha-v2-arclif-developer.vercel.app";
-var api_url = "https://agriha-backend.onrender.com";
+var api_url = "https://agriha-backend-6e2r.onrender.com";
 
 export default api_url;

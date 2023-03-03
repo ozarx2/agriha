6130d05662ba498699eb4d7848bc5b3640b30407
61-5a <azaslam121@gmail.com>
2023-03-03 19:03:53 +0530

    o hided on architect dashboard and visible only number

--- src/utils/endpoint.js
+++ src/utils/endpoint.js
@@ -1,6 +1,7 @@
 // var endpoint = "https://agrihav2-test.onrender.com";
 // var endpoint = "https://agriha-staging.onrender.com";
 // var endpoint = "https://agriha-v2-arclif-developer.vercel.app";
-var endpoint = "https://agriha-backend.onrender.com";
+// var endpoint = "https://agriha-backend.onrender.com";
+var endpoint = "https://agriha-backend-6e2r.onrender.com";
 
 export default endpoint;

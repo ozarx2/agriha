fae58c83708d2d6d53efc67d3a9653e4ef299a0d
aZ <94156430+61-5a@users.noreply.github.com>
2023-03-30 18:15:33 +0530

    url change

--- src/utils/url.js
+++ src/utils/url.js
@@ -2,7 +2,7 @@
 // var api_url = "https://agriha-staging.onrender.com";
 // var api_url = "https://agriha-v2-arclif-developer.vercel.app";
 // var api_url = "https://agriha-backend-6e2r.onrender.com";
-
-var api_url = "https://agrihav2-production.up.railway.app";
+// var api_url = "https://agrihav2-production.up.railway.app";
+var api_url = "https://web-production-e4da.up.railway.app";
 
 export default api_url;
